d26597cd02617829cbee5bf35dd640128ef22d8a
Sayemhaque <ssayem846@gmail.com>
2023-02-02 02:56:28 +0600

    learing js Math

---
+++ Math.js
@@ -0,0 +1,14 @@
+//get power of a number
+
+const powNum = Math.pow(3,2);
+console.log(powNum) // output : 9; ---> 3*3
+
+
+//get absolute of a number
+console.log(Math.abs(30 - 43))  //expected output: -13  //actual output: 13;
+
+//get random number equal to 0 less than 1 
+console.log(Math.round(Math.random() * 7))
+
+console.log(Math.floor(34.43))
+console.log(Math.ceil(34.43))

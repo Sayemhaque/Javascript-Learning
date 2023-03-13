c9dc413f285c1031f534a1061b6fd6e97cdb6702
unknown <sayemhaque>
2023-03-13 21:53:23 +0600

    learing destructuring

---
+++ arrayDestructuring.js
@@ -0,0 +1,15 @@
+const  products = 
+    {
+        id:1,
+        name:"I phone 13max pro",
+        category:"Electronic",
+        colors:["Red" , "Green" , "Blue"]
+    }
+
+
+
+
+const [firstColor,secondColor,thirdColor] = products.colors;
+
+
+console.log(firstColor)
